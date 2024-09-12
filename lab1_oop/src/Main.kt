1463acea86b39b35706e542ed3251f88e1390f16
erdswgtere <maxxim200508@gmail.com>
2024-09-12 21:54:09 +0300

    Программа по ООП закончена

--- lab1_oop/src/Main.kt
+++ lab1_oop/src/Main.kt
@@ -19,13 +19,16 @@ fun main() {
     val a = readln().toFloat()
     print("Введите b: ")
     val b = readln().toFloat()
-    val twoVec =  twoBeginVectors(x1, y1, x2, y2, a, b)
+    val twoVec =  TwoBeginVectors(x1, y1, x2, y2, a, b)
+    twoVec.parallelogramSquare()
+    println("Координаты середины двух векторов: ")
+    twoVec.middleVectorCord()
 }
 open class VectorOnPlane(val x1: Float, val y1: Float, val x2: Float, val y2: Float){
    open fun middleVectorCord(){
         val nX = (x1 + x2) / 2
         val nY = (y1 + y2) / 2
-        println("Кординаты середины вектора равны: $nX, $nY")
+        println("Координаты середины вектора равны: $nX, $nY")
     }
     open fun definition45degree(){
         val angel = atan((y2-y1)/(x2-x1))
@@ -38,14 +41,21 @@ open class VectorOnPlane(val x1: Float, val y1: Float, val x2: Float, val y2: Fl
         }
     }
 }
-class twoBeginVectors(x1: Float, y1: Float, x2: Float, y2: Float, val a: Float, val b: Float):VectorOnPlane(x1, y1, x2, y2){
-    fun parallelogram_square(){
+class TwoBeginVectors(x1: Float, y1: Float, x2: Float, y2: Float, val a: Float, val b: Float):VectorOnPlane(x1, y1, x2, y2){
+    fun parallelogramSquare(){
         val proiz = x1 * a * x1 + y1 * b * y1 + x2 * a * x2 + y2 * b * x2
         val len1 = sqrt(x1.pow(2)+y1.pow(2)+x2.pow(2)+y2.pow(2))
-        val len2 = sqrt(a * x1.pow(2)+ b * y1.pow(2)+a * x2.pow(2)+ b* y2.pow(2))
+        val len2 = sqrt( a * x1.pow(2)+ b * y1.pow(2)+a * x2.pow(2)+ b * y2.pow(2))
         val cosines = proiz / (len1 * len2)
         val sinus = 1 - cosines.pow(2)
         val s = len1 * len2 * sinus
         println("Площадь параллелограмма равна: $s")
+    }
+
+    override fun middleVectorCord() {
+        super.middleVectorCord()
+        val nX2 = (a*x1 + a * x2) / 2
+        val nY2 = (b*y1 + b * y2) / 2
+        println("Координаты середины второго вектора равны: $nX2, $nY2")
     }
 }
