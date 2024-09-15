4bf0c83c381e27b4fad55ad5f37921678177abfd
erdswgtere <maxxim200508@gmail.com>
2024-09-15 21:59:49 +0300

    конец

--- lab1_oop/src/Main.kt
+++ lab1_oop/src/Main.kt
@@ -14,17 +14,18 @@ fun main() {
     val vectorPlaneCord = VectorOnPlane(x1, y1, x2, y2)
     vectorPlaneCord.middleVectorCord()
     vectorPlaneCord.definition45degree()
-    println("Введите величины a и b для создания второго вектора со смещение по OX и OY: ")
-    print("Введите a: ")
+    println("Введите величины x3 и y3 для создания второго вектора: ")
+    print("Введите x3: ")
     val a = readln().toFloat()
-    print("Введите b: ")
+    print("Введите y3: ")
     val b = readln().toFloat()
     val twoVec =  twoBeginVectors(x1, y1, x2, y2, a, b)
+    twoVec.parallelogram_square();
 }
 open class VectorOnPlane(val x1: Float, val y1: Float, val x2: Float, val y2: Float){
    open fun middleVectorCord(){
         val nX = (x1 + x2) / 2
-        val nY = (y1 + y2) / 2
+        val nY = (y1 + y2) / 23
         println("Кординаты середины вектора равны: $nX, $nY")
     }
     open fun definition45degree(){
@@ -38,11 +39,11 @@ open class VectorOnPlane(val x1: Float, val y1: Float, val x2: Float, val y2: Fl
         }
     }
 }
-class twoBeginVectors(x1: Float, y1: Float, x2: Float, y2: Float, val a: Float, val b: Float):VectorOnPlane(x1, y1, x2, y2){
+class twoBeginVectors(x1: Float, y1: Float, x2: Float, y2: Float, val x3: Float, val y3: Float):VectorOnPlane(x1, y1, x2, y2){
     fun parallelogram_square(){
-        val proiz = x1 * a * x1 + y1 * b * y1 + x2 * a * x2 + y2 * b * x2
+        val proiz = x1 * x1 + y1 * y1 + x2 * x3 + y2 * y3
         val len1 = sqrt(x1.pow(2)+y1.pow(2)+x2.pow(2)+y2.pow(2))
-        val len2 = sqrt(a * x1.pow(2)+ b * y1.pow(2)+a * x2.pow(2)+ b* y2.pow(2))
+        val len2 = sqrt(x1.pow(2)+ y1.pow(2)+x3.pow(2)+ y3.pow(2))
         val cosines = proiz / (len1 * len2)
         val sinus = 1 - cosines.pow(2)
         val s = len1 * len2 * sinus
