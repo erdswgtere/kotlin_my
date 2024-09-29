2b8972b31bb2d406b27f979249e3f43782201a97
erdswgtere <maxxim200508@gmail.com>
2024-09-29 20:11:09 +0300

    lr 5 end

--- lab5_TextView_editTextNumber_Button/calc/app/src/main/java/com/example/calc/MainActivity.kt
+++ lab5_TextView_editTextNumber_Button/calc/app/src/main/java/com/example/calc/MainActivity.kt
@@ -16,24 +16,22 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         val textView: TextView = findViewById(R.id.textView)
-        // Регистрация прослушивателя для button
+
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
             textView.text = "Нажата кнопка 1"
         }
-        //--------------------------------------------
-        // Регистрация прослушивателя для button2
+
         findViewById<Button>(R.id.button2).setOnClickListener {
             displayTextOnButtonClick(it)
         }
-        //--------------------------------------------
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-    // Метод обработчика событий
     private fun displayTextOnButtonClick(view: View) {
         val text = findViewById<TextView>(R.id.textView)
         text.text = "Нажата кнопка 2"
